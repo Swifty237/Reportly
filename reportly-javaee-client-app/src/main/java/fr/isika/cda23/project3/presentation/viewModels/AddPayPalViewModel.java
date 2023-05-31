2b53d75e9c63991db7dd8594ef0816cc95b811c6
scezen <nootybear@gmail.com>
2023-05-31 17:49:23 +0200

    PaypalDAO

---
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/viewModels/AddPayPalViewModel.java
@@ -0,0 +1,31 @@
+package fr.isika.cda23.project3.presentation.viewModels;
+
+public class AddPayPalViewModel {
+
+	private String EmailPaypal;
+	private String PasswordPaypal;
+	
+	public String getEmailPaypal() {
+		return EmailPaypal;
+	}
+	public void setEmailPaypal(String emailPaypal) {
+		EmailPaypal = emailPaypal;
+	}
+	public String getPasswordPaypal() {
+		return PasswordPaypal;
+	}
+	public void setPasswordPaypal(String passwordPaypal) {
+		PasswordPaypal = passwordPaypal;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("AddPayPalViewModel [EmailPaypal=");
+		builder.append(EmailPaypal);
+		builder.append(", PasswordPaypal=");
+		builder.append(PasswordPaypal);
+		builder.append("]");
+		return builder.toString();
+	}
+}
--- reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/repository/payment/PaypalDao.java
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/repository/payment/PaypalDao.java
@@ -1,5 +1,44 @@
 package fr.isika.cda23.project3.repository.payment;
 
-public class PaypalDao {
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
-}
+import fr.isika.cda.entities.esn.BankCard;
+import fr.isika.cda23.project3.presentation.viewModels.AddBankCardViewModel;
+
+	@Stateless
+	public class PaypalDao {
+		
+		@PersistenceContext
+		private EntityManager entityManager;
+		
+		public Long addPaypal(AddPayPalViewModel appvm) {
+			
+			BankCard bankcard = new BankCard();
+			
+			bankcard.setOwnerName(appvm.getOwnerName());
+			bankcard.setBankCardNumber(appvm.getBankCardNumber());
+			bankcard.setSecurityNumber(appvm.getSecurityNumber());
+			bankcard.setDateExpiration(appvm.getDateExpiration());
+			
+			entityManager.persist(bankcard);
+			
+			System.out.println("Carte : " + appvm.toString() + " persistée");
+			
+			return bankcard.getId();
+		}
+		
+		public void removeBankCard(BankCard bankcard) {
+			entityManager.remove(bankcard);
+			System.out.println("Carte bancaire supprimée ================================================");
+		}
+		
+		public void updateBankCard(BankCard bankcard) {
+			// A faire en fonction de la propriété (ou des propriétés) qu'on aura besoin de modifier
+		}
+		
+		public BankCard findBankCardById(Long id) {
+		    return entityManager.getReference(BankCard.class, id);
+		}
+	}
