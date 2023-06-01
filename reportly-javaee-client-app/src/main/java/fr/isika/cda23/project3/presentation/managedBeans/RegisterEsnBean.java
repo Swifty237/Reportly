3d333c71b3678cc74917fd8ba3ae8286ac7e1f4b
zhang <jinz0920@yahoo.fr>
2023-06-01 15:44:35 +0200

    user

---
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/managedBeans/RegisterEsnBean.java
@@ -0,0 +1,29 @@
+package fr.isika.cda23.project3.presentation.managedBeans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+import fr.isika.cda23.project3.business.RegisterEsnService;
+import fr.isika.cda23.project3.presentation.viewModels.RegisterEsnViewModel;
+
+@ManagedBean
+@SessionScoped
+public class RegisterEsnBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5469910363275392680L;
+
+	@Inject
+	private RegisterEsnService reService;
+
+	private RegisterEsnViewModel registerEsnVm = new RegisterEsnViewModel();
+
+	public void register() {
+		reService.register(registerEsnVm);
+	}
+}
