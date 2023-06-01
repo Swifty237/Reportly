b7c6b2aa014e1693a0797ae82e9553771ffaaa5b
zhang <jinz0920@yahoo.fr>
2023-06-01 15:44:35 +0200

    user

---
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/viewModels/RegisterEsnViewModel.java
@@ -0,0 +1,69 @@
+package fr.isika.cda23.project3.presentation.viewModels;
+
+public class RegisterEsnViewModel {
+
+	private String brand;
+	private String email;
+	private String password;
+	private String adress;
+	private String city;
+	private String postalCode;
+	private String country;
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
