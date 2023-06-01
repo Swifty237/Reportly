54dbdcdc3bcfa2f17f3a89a1d940dc9ead3bff60
zhang <jinz0920@yahoo.fr>
2023-06-01 15:44:35 +0200

    user

---
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/business/RegisterEsnService.java
@@ -0,0 +1,28 @@
+package fr.isika.cda23.project3.business;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import fr.isika.cda.entities.common.AdressDetails;
+import fr.isika.cda.entities.esn.Esn;
+import fr.isika.cda23.project3.presentation.viewModels.RegisterEsnViewModel;
+import fr.isika.cda23.project3.repository.EsnDao;
+import fr.isika.cda23.project3.repository.user.UserDao;
+
+@Stateless
+public class RegisterEsnService {
+
+	@Inject
+	private EsnDao esnDao;
+
+	public void register(RegisterEsnViewModel viewModel) {
+		Esn esn = new Esn();
+		
+		AdressDetails adressDetails = new AdressDetails();
+		adressDetails.setAdress(viewModel.getAdress());
+		adressDetails.setCity(viewModel.getCity());
+		adressDetails.setCountry(viewModel.getCountry());
+		adressDetails.setPostalCode(viewModel.getPostalCode());
+
+	}
+}
