5f75810616f3dab09ef1a978f4ff8328aa950e05
scezen <nootybear@gmail.com>
2023-06-02 21:15:26 +0200

    Latest changes

--- reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/managedBeans/ExpenseReportBean.java
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/managedBeans/ExpenseReportBean.java
@@ -1,17 +1,20 @@
 package fr.isika.cda23.project3.presentation.managedBeans;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
+import fr.isika.cda.entities.common.ExpenseReport;
 import fr.isika.cda23.project3.presentation.viewModels.ExpenseReportViewModel;
 import fr.isika.cda23.project3.repository.common.ExpenseReportDao;
 
 @ManagedBean(name = "expenseBean")
-@ViewScoped
-public class ExpenseReportBean implements Serializable{
+@SessionScoped
+public class ExpenseReportBean implements Serializable {
 
 	/**
 	 * 
@@ -20,12 +23,42 @@ public class ExpenseReportBean implements Serializable{
 
 	@Inject
 	private ExpenseReportDao expenseReportDao;
-	
-	private ExpenseReportViewModel expenseReportViewModel = new ExpenseReportViewModel();
-	
-	public String addExpenseReport() {
-	expenseReportDao.addExpenseReport(expenseReportViewModel);	
-	expenseReportViewModel = new ExpenseReportViewModel();
-	return "expenseReport.xhtml";
+
+	private ExpenseReportViewModel expenseReportVm = new ExpenseReportViewModel();
+	private List<ExpenseReport> expenseReports;
+
+	    public void setExpenseReportDao(ExpenseReportDao expenseReportDao) {
+	        this.expenseReportDao = expenseReportDao;
+	    }
+
+	    public ExpenseReportViewModel getExpenseReport() {
+	        return expenseReportVm;
+	    }
+
+	    public void setExpenseReport(ExpenseReportViewModel expenseReport) {
+	        this.expenseReportVm = expenseReport;
+	    }
+
+	    public List<ExpenseReport> getExpenseReports() {
+	        if (expenseReports == null) {
+	            expenseReports = expenseReportDao.getAllExpenseReports();
+	        }
+	        return expenseReports;
+	    }
+
+	    public void addExpenseReport() {
+	        Long id = expenseReportDao.addExpenseReport(expenseReportVm);
+	        expenseReportVm.setId(id);
+	        expenseReports = null; // Invalidate the cached expenseReports list
+	        expenseReportVm = new ExpenseReportViewModel();
+	    }
+
+	    public void updateExpenseReport(ExpenseReportViewModel ervm) {
+	        expenseReportDao.updateExpenseReport(ervm);
+	    }
+
+	    public void deleteExpenseReport(Long id) {
+	        expenseReportDao.deleteExpenseReport(id);
+	        expenseReports = null; // Invalidate the cached expenseReports list
+	    }
 	}
-}
