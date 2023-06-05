8d301f0ea7ae5898dba8072d306390f6ecb4a2eb
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-06-05 12:57:46 +0200

    update companyDetailsViewModel

--- reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/viewModels/CompanyDetailsViewModel.java
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/viewModels/CompanyDetailsViewModel.java
@@ -4,21 +4,28 @@ import java.util.Date;
 
 public class CompanyDetailsViewModel {
 	
-	
-	private Date CreationDate = new Date();
-	private String Brand;
-	private String Email;
-	
+	private Date creationDate = new Date();
+	private String brand;
+	private String email;
 	
 	public Date getCreationDate() {
-		return CreationDate;
+		return creationDate;
 	}
-
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
 	public String getBrand() {
-		return Brand;
+		return brand;
 	}
-	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
 	public String getEmail() {
-		return Email;
+		return email;
 	}
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+	
+	
 }
