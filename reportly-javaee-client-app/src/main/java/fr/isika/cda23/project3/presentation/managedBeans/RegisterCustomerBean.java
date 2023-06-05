986f9c8d48dc11cf8d60a13e89b5d4da9edf72fd
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-06-05 05:35:01 +0200

    bug on RegisterCustomerBean fixed

--- reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/managedBeans/RegisterCustomerBean.java
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/managedBeans/RegisterCustomerBean.java
@@ -3,18 +3,23 @@ package fr.isika.cda23.project3.presentation.managedBeans;
 
 
 import java.io.IOException;
+import java.io.Serializable;
 
-import javax.annotation.ManagedBean;
-
+import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 
 import fr.isika.cda23.project3.business.RegisterCustomerService;
 import fr.isika.cda23.project3.presentation.viewModels.RegisterCustomerViewModel;
 import fr.isika.cda23.project3.utils.NavigationUtils;
 
-
 @ManagedBean
-public class RegisterCustomerBean {
+public class RegisterCustomerBean implements Serializable {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2698259096055665222L;
 
 
 	@Inject
