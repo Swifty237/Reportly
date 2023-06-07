3281dae98ec35703e195bfc435e0ce36d58b2e05
scezen <nootybear@gmail.com>
2023-06-07 17:20:17 +0200

    Expense Reports loading from DBB

--- reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/managedBeans/ExpenseReportBean.java
+++ reportly-javaee-client-app/src/main/java/fr/isika/cda23/project3/presentation/managedBeans/ExpenseReportBean.java
@@ -1,9 +1,6 @@
 package fr.isika.cda23.project3.presentation.managedBeans;
- 
+
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,81 +8,61 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 
-import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.file.UploadedFile;
 
 import fr.isika.cda.entities.common.DocumentType;
+import fr.isika.cda.entities.common.ExpenseReport;
 import fr.isika.cda.entities.common.ExpenseReportState;
 import fr.isika.cda23.project3.presentation.viewModels.ExpenseReportViewModel;
 import fr.isika.cda23.project3.repository.common.ExpenseReportDao;
-import fr.isika.cda23.project3.utils.FileUploadUtils;
- 
+
 @ManagedBean
 @SessionScoped
 public class ExpenseReportBean implements Serializable {
- 
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 6669005664250781823L;
- 
-	@Inject
-	private ExpenseReportDao expenseReportDao;
- 
-	private List<ExpenseReportViewModel> expenseReports;
+
+    private static final long serialVersionUID = 6669005664250781823L;
+
+    @Inject
+    private ExpenseReportDao expenseReportDao;
+
+    private List<ExpenseReport> expenseReports;
     private ExpenseReportViewModel expenseReportViewModel;
     
     private UploadedFile uploadedFile;
- 
+
     @PostConstruct
     public void init() {
-        expenseReports = new ArrayList<>();
+        expenseReports = expenseReportDao.getAllExpenseReports();
         expenseReportViewModel = new ExpenseReportViewModel();
     }
- 
+
     public void addExpenseReport() {
-    	expenseReportViewModel.setExpenseReportState(ExpenseReportState.PENDING);
-    	
-        // Add the expense report to the list
-        expenseReports.add(expenseReportViewModel);
-        expenseReportDao.addExpenseReport(expenseReportViewModel);
- 
+        expenseReportViewModel.setExpenseReportState(ExpenseReportState.PENDING);
+
+        // Persist the expense report
+        Long id = expenseReportDao.addExpenseReport(expenseReportViewModel);
+
+        // Update the expenseReports list
+        ExpenseReport expenseReport = expenseReportDao.getExpenseReportById(id);
+        expenseReports.add(expenseReport);
+
         // Reset the form
         expenseReportViewModel = new ExpenseReportViewModel();
-        uploadedFile = null;
     }
- 
-    public void uploadFile(FileUploadEvent event) throws Exception {
-    	String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyyHHmmss"));
- 
-    	UploadedFile uploadedFile = event.getFile();
-		String fileName = String.join("_", timestamp, uploadedFile.getFileName());
- 
-		expenseReportViewModel.setFileName(fileName);
- 
-		FileUploadUtils.uploadFileToApp(uploadedFile, fileName);
-    }
-    
-    public Integer countExpenseReports() {
-    	return expenseReportDao.getAllExpenseReports().size();
-    }
-    
-    public void deleteExpenseReport(ExpenseReportViewModel expenseReport) {
+
+    public void deleteExpenseReport(ExpenseReport expenseReport) {
+        expenseReportDao.deleteExpenseReport(expenseReport);
         expenseReports.remove(expenseReport);
     }
- 
-    public void editExpenseReport(ExpenseReportViewModel expenseReport) {
-        expenseReportViewModel = expenseReport;
-    }
- 
-    public List<ExpenseReportViewModel> getExpenseReports() {
+
+    public List<ExpenseReport> getExpenseReports() {
         return expenseReports;
     }
- 
+
     public ExpenseReportViewModel getExpenseReportViewModel() {
         return expenseReportViewModel;
     }
- 
+    
     public UploadedFile getUploadedFile() {
         return uploadedFile;
     }
@@ -98,4 +75,3 @@ public class ExpenseReportBean implements Serializable {
     	return DocumentType.values();
     }
 }
- 
